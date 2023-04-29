e08a1d020de378d1273602491f134bdc0ffa3098
makanta <mktmakantamkt@gmail.com>
2023-04-29 19:26:57 +0100

    adding span

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const openNow = document.querySelectorAll('.open');
-
+const span = document.querySelector('span');
   openNow.forEach( (opens) => {
     opens.addEventListener('click', () => {
       opens.classList.toggle('now');
+      span.style.transform = rotate("180'deg")
+
     });
   });
